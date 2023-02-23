a5a812c546c5b46ff57b5b2a2b8ab448f0b36ad9
pskrebnev <Kumbaya33>
2023-02-24 00:11:28 +0100

    Single responsibilities: added changes in main() method

--- src/main/java/org/simple/designpatterns/solid/singleresponcibilities/SprDemo.java
+++ src/main/java/org/simple/designpatterns/solid/singleresponcibilities/SprDemo.java
@@ -7,6 +7,7 @@ public class SprDemo {
         Journal journal = new Journal();
         journal.addEntry("First things");
         journal.addEntry("Second things");
+        journal.addEntry("Another row");
 
         System.out.println(journal);
 
@@ -14,7 +15,7 @@ public class SprDemo {
 
         String filename = "src/main/resources/list.txt";
         try {
-            persistence.saveToFile(journal, filename, true);
+            persistence.saveToFile(journal, filename, false);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
