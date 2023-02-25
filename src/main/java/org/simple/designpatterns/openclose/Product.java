cb1c3c681dc9f72f5ca5ce151ee8991bd9a3143f
pskrebnev <Kumbaya33>
2023-02-25 17:08:37 +0100

    Open / Close principles (bad example)

--- src/main/java/org/simple/designpatterns/openclose/Product.java
+++ src/main/java/org/simple/designpatterns/openclose/Product.java
@@ -1,5 +1,14 @@
 package org.simple.designpatterns.openclose;
 
 public class Product {
+    public String name;
+    public Color color;
+    public Size size;
+
+    public Product(String name, Color color, Size size) {
+        this.name = name;
+        this.color = color;
+        this.size = size;
+    }
 
 }
